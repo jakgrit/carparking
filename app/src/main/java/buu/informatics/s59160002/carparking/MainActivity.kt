7a3932e96991799ab72f249747cdb9dc615c20f2
Jakgrit <jakgrit_frame@hotmail.com>
2019-08-22 11:32:26 +0700

    little success

--- app/src/main/java/buu/informatics/s59160002/carparking/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59160002/carparking/MainActivity.kt
@@ -13,6 +13,8 @@ class MainActivity : AppCompatActivity() {
     private var car2: Car = Car()
     private var car3: Car = Car()
 
+    private var check: String = ""
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +23,9 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.slot1_button).setOnClickListener { checkBtn(it, "1") }
         findViewById<Button>(R.id.slot2_button).setOnClickListener { checkBtn(it, "2") }
         findViewById<Button>(R.id.slot3_button).setOnClickListener { checkBtn(it, "3") }
+
+        findViewById<Button>(R.id.save_button).setOnClickListener { initValue() }
+        findViewById<Button>(R.id.cancel_button).setOnClickListener { initValue() }
 
         disableInput()
     }
@@ -56,11 +61,67 @@ class MainActivity : AppCompatActivity() {
 
     private fun checkBtn(view: View, idx: String){
         if(idx.equals("1")){
-
+            enableInput()
+            check = "1"
         }else if(idx.equals("2")){
-
+            enableInput()
+            check = "2"
         }else if(idx.equals("3")){
+            enableInput()
+            check = "3"
+        }
+        clearInput()
+    }
 
+    private fun initValue(){
+        val carID = findViewById<EditText>(R.id.carId_editText)
+        val firstName = findViewById<EditText>(R.id.name_editText)
+        val lastName = findViewById<EditText>(R.id.surname_editText)
+
+        if(check == "1"){
+            car1.carId = carID.text.toString()
+            car1.name = firstName.text.toString()
+            car1.surName = lastName.text.toString()
+        }else if(check == "2"){
+            car2.carId = carID.text.toString()
+            car2.name = firstName.text.toString()
+            car2.surName = lastName.text.toString()
+        }else{
+            car3.carId = carID.text.toString()
+            car3.name = firstName.text.toString()
+            car3.surName = lastName.text.toString()
+        }
+        clearInput()
+        showText()
+    }
+
+    private fun clearInput(){
+        val carID = findViewById<EditText>(R.id.carId_editText)
+        val firstName = findViewById<EditText>(R.id.name_editText)
+        val lastName = findViewById<EditText>(R.id.surname_editText)
+
+        carID.setText("")
+        firstName.setText("")
+        lastName.setText("")
+    }
+
+    private fun showText(){
+        val carID = findViewById<EditText>(R.id.carId_editText)
+        val firstName = findViewById<EditText>(R.id.name_editText)
+        val lastName = findViewById<EditText>(R.id.surname_editText)
+
+        if(check == "1"){
+            carID.setText(car1.carId)
+            firstName.setText(car1.name)
+            lastName.setText(car1.surName)
+        }else if(check == "2"){
+            carID.setText(car2.carId)
+            firstName.setText(car2.name)
+            lastName.setText(car2.surName)
+        }else{
+            carID.setText(car3.carId)
+            firstName.setText(car3.name)
+            lastName.setText(car3.surName)
         }
     }
 }
